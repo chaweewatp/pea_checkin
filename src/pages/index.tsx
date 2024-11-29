85ca612ba7c852651fdf5ce7128bf9f6360e2b93
pornchai <chaweewat.p@gmail.com>
2024-11-29 12:07:11 +0700

    edit zoom 20

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -67,20 +67,17 @@ export default function Home() {
               lat: location.latitude,
               lng: location.longitude,
             }}
-            zoom={15}
+            zoom={20}
           >
-            {/* Marker for the user's location */}
             <Marker
               position={{ lat: location.latitude, lng: location.longitude }}
             />
-
-            {/* Circle representing the accuracy */}
             <Circle
               center={{
                 lat: location.latitude,
                 lng: location.longitude,
               }}
-              radius={location.accuracy} // Set the radius using accuracy
+              radius={location.accuracy} 
               options={{
                 fillColor: "rgba(0, 123, 255, 0.2)",
                 strokeColor: "rgba(0, 123, 255, 0.5)",
